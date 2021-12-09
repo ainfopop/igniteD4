0649c35afe0840d52dd787274d8a6e95e842ccbf
Rodrigo De Oliveira <casa.rodrigo@gmail.com>
2021-12-09 10:37:34 -0300

    Resolução Desafio Ignite 4

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if ((!user) || (!user.admin)) {
+      throw new Error("User not permission!");
+    }
+
+    const listUsers = this.usersRepository.list(); 
+
+    return listUsers;
   }
 }
 
